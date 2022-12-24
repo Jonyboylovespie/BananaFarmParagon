c6af8a5925556a54054de8e9e42dd95d25a5607a
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2022-12-24 14:53:38 -0500

    Add files via upload

--- BananaFarmParagon.cs
+++ BananaFarmParagon.cs
@@ -7,16 +7,12 @@ using Assets.Scripts.Unity.Display;
 using BTD_Mod_Helper;
 using BTD_Mod_Helper.Api;
 using BTD_Mod_Helper.Api.Display;
-using BTD_Mod_Helper.Api.Enums;
 using BTD_Mod_Helper.Api.Towers;
 using BTD_Mod_Helper.Extensions;
-using System;
 using MelonLoader;
 using ModHelperData = BananaFarmParagon.ModHelperData;
 using BTD_Mod_Helper.Api.ModOptions;
-using System.IO;
 using Assets.Scripts.Unity.UI_New.Popups;
-using Action = System.Action;
 using BananaFarmParagon.bananafarmfake;
 using Assets.Scripts.Models.Towers.Behaviors.Attack.Behaviors;
 using Assets.Scripts.Utils;
@@ -26,9 +22,7 @@ using BTD_Mod_Helper.Api.Data;
 using Assets.Scripts.Models;
 using Assets.Scripts.Models.Towers.Mods;
 using BTD_Mod_Helper.Api.Helpers;
-using Assets.Scripts.Models.Towers.Behaviors.Abilities;
-using System.Collections.Generic;
-using Il2CppSystem.Collections.Generic;
+using Assets.Scripts.Models.TowerSets;
 
 [assembly: MelonInfo(typeof(BananaFarmParagon.Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -91,7 +85,7 @@ namespace BananaFarmParagon
             
                 if (Settings.ParagonTowerOp == false)
                 {
-                    MelonLogger.Msg(ConsoleColor.Yellow, "The balanced version of the banana farm paragon has been loaded.");
+                    MelonLogger.Msg(System.ConsoleColor.Yellow, "The balanced version of the banana farm paragon has been loaded.");
                     if (Settings.TogglePopup == true)
                     {
                         PopupScreen.instance.ShowOkPopup("The balanced version of the banana farm paragon has been loaded.");
@@ -99,7 +93,7 @@ namespace BananaFarmParagon
                 }
                 else
                 {
-                    MelonLogger.Msg(ConsoleColor.Yellow, "The OP version of the banana farm paragon has been loaded.");
+                    MelonLogger.Msg(System.ConsoleColor.Yellow, "The OP version of the banana farm paragon has been loaded.");
                     if (Settings.TogglePopup == true)
                     {
                         PopupScreen.instance.ShowOkPopup("The OP version of the banana farm paragon has been loaded.");
@@ -109,7 +103,7 @@ namespace BananaFarmParagon
         }
         public override void OnApplicationStart()
         {
-            MelonLogger.Msg(ConsoleColor.Yellow, "The great one... Is ready...");
+            MelonLogger.Msg(System.ConsoleColor.Yellow, "The great one... Is ready...");
         }
     }
     public class Settings : ModSettings
@@ -196,12 +190,10 @@ namespace BananaFarmParagon
         {
             public override string Portrait => "BananaCostumeFarmerPortrait";
             public override string Name => "Banan";
-            public override string TowerSet => TowerSetType.Support;
+            public override TowerSet TowerSet => TowerSet.Support;
             public override string BaseTower => TowerType.DartMonkey;
-
             public override bool DontAddToShop => true;
             public override int Cost => 0;
-
             public override int TopPathUpgrades => 0;
             public override int MiddlePathUpgrades => 0;
             public override int BottomPathUpgrades => 0;
