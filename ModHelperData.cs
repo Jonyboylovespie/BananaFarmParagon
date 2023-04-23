d7fd0025e0372079ac0acf7b5d7d7ea0a7c8801f
Jonyboylovespie <90563990+Jonyboylovespie@users.noreply.github.com>
2023-04-23 13:05:20 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,7 +2,7 @@
 {
     public static class ModHelperData
     {
-        public const string WorksOnVersion = "34.3";
+        public const string WorksOnVersion = "36.3";
         public const string Version = "1.0.5";
         public const string Name = "Banana Farm Paragon";
         public const string Description =
